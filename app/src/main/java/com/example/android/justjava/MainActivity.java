e69de1decd63c69f31f12702ca445381ad84f9a1
ccojocea <ccojocea@gmail.com>
2018-01-12 19:34:49 +0200

    Various changes - Lesson11 - Quiz11

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,19 +1,3 @@
-/*
-package com.example.android.justjava;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
-*/
-
 /**
  * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
  * This is the package name our example uses:
@@ -24,12 +8,14 @@ public class MainActivity extends AppCompatActivity {
 
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
 
 
 /**
@@ -37,13 +23,17 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 2;
+    int quantity;
     int pricePerCup = 5;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView quantityView = findViewById(R.id.quantity_text_view);
+        quantity = Integer.valueOf(quantityView.getText().toString());
     }
 
     /**
@@ -68,16 +58,43 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        int price = calculatePrice();
+
+        CheckBox whippedCream = findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCream.isChecked();
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        EditText nameEditText = findViewById(R.id.name_edit_text);
+        name = nameEditText.getText().toString();
+
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        displayMessage(priceMessage);
 
 //        Locale locale=Locale.getDefault();
 //        Currency currency=Currency.getInstance(locale);
 //        String symbol = currency.getSymbol();
 //        String priceMessage = "Total: " + symbol + price;
+//        String priceMessage = "Total: "+ "$" + price;
+//        priceMessage = priceMessage + "\n" + "Thank you!";
+    }
 
-        int price = calculatePrice();
-        String priceMessage = "Total: "+ "$" + price;
-        priceMessage = priceMessage + "\n" + "Thank you!";
-        displayMessage(priceMessage);
+    /**
+     * Create summary of the order.
+     *
+     * @param hasWhippedCream is whether or not the user wants whipped cream topping
+     * @param hasChocolate is whether or not the user wants chocolate topping
+     * @param price of the order
+     * @return text summary
+     */
+
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate){
+        String orderSummary = "Name: " + name;
+        orderSummary += "\nAdd whipped cream? " + hasWhippedCream;
+        orderSummary += "\nAdd chocolate? " + hasChocolate;
+        orderSummary += "\nQuantity: " + quantity;
+        orderSummary += "\nTotal: "+"$"+ price;
+        orderSummary += "\nThank you!";
+        return orderSummary;
     }
 
     /**
@@ -91,26 +108,29 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     public void displayMessage(String message){
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
     private void displayQuantity(int nr) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + nr);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    public void goToWeb(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+        startActivity(browserIntent);
     }
 
-
+//    /**
+//     * This method displays the given price on the screen.
+//     */
+//    private void displayPrice(int number) {
+//        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+//        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+//    }
 
 }
